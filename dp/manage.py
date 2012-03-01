df5d6daa499f7015b01c593fe06d62e82e639f15
Chris Jones <chris@brack3t.com>
2012-02-29 16:57:16 -0800

    Fix manage.py. Enable admin.

--- dp/manage.py
+++ dp/manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import imp
+import sys
+from os.path import join
 
 from django.core.management import execute_manager, setup_environ
 
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
