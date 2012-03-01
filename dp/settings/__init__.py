d360db40530ae7649e74f792159d736b42dd6f0f
Chris Jones <chris@brack3t.com>
2012-02-29 17:10:17 -0800

    static.

--- dp/settings/__init__.py
+++ dp/settings/__init__.py
@@ -1,6 +1,11 @@
 from .base import *
 
 try:
+    from .secret import *
+except ImportError:
+    pass
+
+try:
     from .local import *
 except ImportError:
     pass
--- dp/urls.py
+++ dp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
