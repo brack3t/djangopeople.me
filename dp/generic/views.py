51a3bb75d66bf2bc208896c4210eda906fb4c9a2
Kenneth Love <kenneth@gigantuan.net>
2012-05-28 11:03:45 -0700

    moved templates

--- dp/generic/views.py
+++ dp/generic/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "index.html"
+    template_name = "generic/index.html"
 
     def get(self, request, *args, **kwargs):
         return self.render_to_response({})
