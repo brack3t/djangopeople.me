db4df3d3f9a2d83409d51be313c61d5149fb748f
Kenneth Love <kenneth@gigantuan.net>
2012-05-28 14:17:43 -0700

    removed postman urls

--- dp/dp/urls.py
+++ dp/dp/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'', include("social_auth.urls")),
-    (r"^messages/", include("postman.urls")),
 
     url(r"^$", HomePageView.as_view(), name="home"),
     url(r"^profile/$", ProfileView.as_view(), name="profile"),
