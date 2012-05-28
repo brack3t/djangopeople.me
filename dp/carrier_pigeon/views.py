667434537c3cb63af5d4f273448bf7805d4a4687
Chris Jones <chris@brack3t.com>
2012-05-28 14:54:03 -0700

    Proper views and urls for inbox and archive.

--- dp/carrier_pigeon/views.py
+++ dp/carrier_pigeon/views.py
@@ -1,7 +1,29 @@
-from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import ListView
+
+from braces.views import LoginRequiredMixin
+
+from carrier_pigeon.models import Message
 
 
-class InboxListView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse("Hola Bitch!")
+class InboxListView(LoginRequiredMixin, ListView):
+    model = Message
+    template_name = "carrier_pigeon/list.html"
+
+    def get_queryset(self):
+        """
+        Return all inbox messages for this user.
+        """
+        return self.request.user.received_messages.filter(
+            recipient_archived=False
+        )
+
+
+class ArchiveListView(InboxListView):
+
+    def get_queryset(self):
+        """
+        Return all archived messages for this user.
+        """
+        return self.request.user.received_messages.filter(
+            recipient_archived=True
+        )
