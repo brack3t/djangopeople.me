8253ce5d3a7d1fae46bfbd2985b57d53da3f7233
Chris Jones <chris@brack3t.com>
2012-05-28 14:36:47 -0700

    better name for url.

--- dp/carrier_pigeon/urls.py
+++ dp/carrier_pigeon/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from carrier_pigeon.views import InboxListView
 
 urlpatterns = patterns('',
-    url(r"^$", InboxListView.as_view(), name="home")
+    url(r"^$", InboxListView.as_view(), name="inbox")
 )
