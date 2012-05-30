e6bcd8d4c2e0896df200e78a367169cf25e59563
Kenneth Love <kenneth@gigantuan.net>
2012-05-30 15:59:36 -0700

    add extra fields to profile form

--- dp/profiles/forms.py
+++ dp/profiles/forms.py
@@ -6,6 +6,16 @@ from crispy_forms.bootstrap import FormActions
 from profiles.models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+    username = forms.RegexField(label="Username", max_length=30,
+        regex=r'^[\w.@+-]+$', help_text="30 characters or fewer. "
+        "Letters, digits and @/./+/-/_ only.",
+        error_messages = {
+            "invalid": "This value may contain only letters, numbers and "
+                "@/./+/-/_ characters."})
+    email = forms.EmailField(label="Email", help_text="Email address you want "
+        "to receive emails from us at. Leave blank if you don't want emails.",
+        required=False)
+    first_name = forms.CharField(label="Full Name")
 
     class Meta:
         model = UserProfile
@@ -14,15 +24,21 @@ class UserProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Fieldset("Configure your profile",
-                "available_for",
+            Fieldset("Personal Details",
+                "first_name",
+                "username",
+                "email",
+                "gravatar_email",
                 "location",
+                "available_for",
+                "bio",
+            ),
+            Fieldset("Site Details",
                 "github_username",
                 "bitbucket_username",
                 "twitter_username",
                 "linkedin_username",
                 "anonymous_messages",
-                "bio",
                 "tags",
             ),
             FormActions(
@@ -30,3 +46,9 @@ class UserProfileForm(forms.ModelForm):
             )
         )
         super(UserProfileForm, self).__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            user = self.instance.user
+            self.fields["username"].initial = user.username
+            self.fields["email"].initial = user.email
+            self.fields["first_name"].initial = user.first_name
